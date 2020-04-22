6a9963236e5daa76b90b22fa2e26acac5e7d4682
Varun Patil <radialapps@gmail.com>
2020-04-22 13:32:05 +0000

    Add spam and SPF verification

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Displays a warning to roundcube users under some contexts.
 
 ## Types of warnings
 * Email originating from outside your organization
+* Maked as spam in X-Spam-Status
+* Failing Received-SPF
 
 ## Installation
 To install, get the plugin with composer in your roundcube directory
--- banner_warn.php
+++ banner_warn.php
@@ -16,10 +16,17 @@
         function init()
         {
             $this->load_config();
+            $this->add_hook('storage_init', array($this, 'storage_init'));
             $this->add_hook('message_objects', array($this, 'warn'));
         }
 
-        function warn($args)
+        public function storage_init($p)
+        {
+            $p['fetch_headers'] = trim($p['fetch_headers'] . ' ' . strtoupper('X-Spam-Status') . ' ' . strtoupper('Received-SPF'));
+            return $p;
+        }
+
+        public function warn($args)
         {
             $RCMAIL = rcmail::get_instance();
 
@@ -28,7 +35,7 @@
             $message = $args['message'];
 
             // Safety check
-            if ($message === NULL && $message->sender === NULL) {
+            if ($message === NULL || $message->sender === NULL || $message->headers->others === NULL) {
                 return array();
             }
 
@@ -37,7 +44,23 @@
                 array_push($content, '<p class="warning"> This mail
                     originated from outside of your organization. Do not click links or
                     open attachments unless you recognize the sender and know 
-                    the content is safe</p>');
+                    the content is safe.</p>');
+            }
+
+            // Check X-Spam-Status
+            $spamStatus = $message->headers->others['x-spam-status'];
+            if (isset($spamStatus) && (strpos(strtolower($spamStatus), 'yes') === 0)) {
+                array_push($content, '<p class="error"> This mail
+                    has been identified as possible spam. Be extra careful while interacting
+                    with its contents.</p>');
+            }
+
+            // Check Received-SPF
+            $spamStatus = $message->headers->others['received-spf'];
+            if (isset($spamStatus) && (strpos(strtolower($spamStatus), 'pass') !== 0)) {
+                array_push($content, '<p class="error"> The authenticity of the
+                    sender of this email could not be verified. <br> Avoid clicking links,
+                    downloading attachments or replying with personal information.</p>');
             }
 
             return array('content' => $content);
