d3f1ea38576f7432b1a71aeeb3852bd030986a4e
Varun Patil <radialapps@gmail.com>
2020-05-19 04:55:28 +0000

    Space out string concatenation

--- banner_warn.php
+++ banner_warn.php
@@ -42,19 +42,19 @@
 
             // Warn users if mail from outside organization
             if (!preg_match($RCMAIL->config->get('org_email_regex'), $message->sender['mailto'])) {
-                array_push($content, '<div class="notice warning">'.$this->gettext('from_outsite').'</div>');
+                array_push($content, '<div class="notice warning">' . $this->gettext('from_outsite') . '</div>');
             }
 
             // Check X-Spam-Status
             $spamStatus = $message->headers->others['x-spam-status'];
             if (isset($spamStatus) && (strpos(strtolower($spamStatus), 'yes') === 0)) {
-                array_push($content, '<div class="notice error">'.$this->gettext('posible_spam').'</div>');
+                array_push($content, '<div class="notice error">' . $this->gettext('posible_spam') . '</div>');
             }
 
             // Check Received-SPF
             $spamStatus = $message->headers->others['received-spf'];
             if (isset($spamStatus) && (strpos(strtolower($spamStatus), 'pass') !== 0)) {
-                array_push($content, '<div class="notice error">'.$this->gettext('spf_fail').'</div>');
+                array_push($content, '<div class="notice error">' . $this->gettext('spf_fail') . '</div>');
             }
 
             return array('content' => $content);
